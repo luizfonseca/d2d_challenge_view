f0a52207055858f9f95d8066a16facb4a6b7cba8
Luiz Fonseca <eu@luiz.cc>
2017-12-04 14:37:07 +0100

    Allow react app to use ENV variable in the case of endpoints

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     // this.endpoint = 'https://door2door-vehicles.herokuapp.com/vehicles'
-    this.endpoint = 'https://door2door-vehicles.herokuapp.com/vehicles'
+    this.endpoint = process.env.API_URL || 'https://door2door-vehicles.herokuapp.com/vehicles'
     this.state = { markers: [], filteredMarkers: [] }
   }
 
